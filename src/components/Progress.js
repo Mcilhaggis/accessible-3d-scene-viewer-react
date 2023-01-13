2003ce8052dce27c5086878bf34021c0dfdffa92
Rachael McIlhagga <rmcilhagga@tvo.org>
2023-01-13 16:01:16 -0500

    Updates

--- src/components/Progress.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Html, useProgress } from '@react-three/drei'
-
-
-export default function Progress() {
-    // reinstating the percentage loader causes a bad steState render to occur
-    // const { progress } = useProgress()
-    return <Html center>Loading...</Html>
-}
